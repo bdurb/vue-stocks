24881f65dc63e894758b1755ac4aca6d21691d69
bdurb <brian@rpmgreenville.com>
2019-05-16 18:01:22 -0700

    set up initial header for project, imported into app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="container">
-    <router-view />
+    <Header />
+    <div class="row">
+      <div class="col-md-12">
+        <router-view />
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-  
+  components: {
+    Header
+  }
 }
 </script>
 
