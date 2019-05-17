edb31ec40c5ce6e9ed42feccf543efe178fb6523
bdurb <brian@rpmgreenville.com>
2019-05-17 14:03:31 -0700

    set up basic stocks and stock component, mocked with dummy data

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>The Stocks Page</h1>
+  <div>
+    <Stock v-for="stock in stocks"></Stock>
+  </div>
 </template>
+
+<script>
+import Stock from './Stock.vue'
+  export default {
+    data() {
+      return {
+        stocks: [
+          {id: 1, name:'BMW', price: 110},
+          {id: 1, name:'Google', price: 170},
+          {id: 1, name:'Twitter', price: 90},
+          {id: 1, name:'Uber', price: 250},
+        ]
+      }
+    },
+    components: {
+      Stock
+    }
+  }
+</script>
