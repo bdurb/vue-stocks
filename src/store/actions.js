06296f8d271b8e789e165e765f4e4c52dddc66ad
bdurb <brian@rpmgreenville.com>
2019-05-28 15:44:20 -0700

    fixed typo in action

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export const loadData = ({commit}) => {
   Vue.http.get('data.json')
-  .then(res => res.json)
+  .then(res => res.json())
   .then(data => {
     if (data) {
       const stocks = data.stocks
